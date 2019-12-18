68b2e7c548ac7091d892beb14c25a5b20f05504c
Nathan Woodward <nathanrichardwoodward@gmail.com>
2019-12-18 08:25:38 +0000

    Complete day 5

--- 5.1-program.py
+++ 5.1-program.py
@@ -9,6 +9,10 @@ OP_ADD=1
 OP_MUL=2
 OP_READ=3
 OP_WRITE=4
+OP_JUMP_IF_TRUE=5
+OP_JUMP_IF_FALSE=6
+OP_LESS_THAN=7
+OP_EQUALS=8
 
 MODE_POSITION = 0
 MODE_IMMEDIATE = 1
@@ -49,7 +53,10 @@ def run(p, inputs=[]):
         op, mode1, mode2, mode3 = decode_op(p[i])
         if op == OP_TERMINATE:
             break # Done
-        elif op == OP_ADD or op == OP_MUL:
+        elif op == OP_ADD or \
+             op == OP_MUL or \
+             op == OP_LESS_THAN or \
+             op == OP_EQUALS:
             if len(p) - i <= 3:
                 raise RuntimeError("Not enough arguments for binary operator.")
             a = load_parameter(p, i+1, mode1)
@@ -61,6 +68,10 @@ def run(p, inputs=[]):
                 p[j] = a + b
             elif op == OP_MUL:
                 p[j] = a * b
+            elif op == OP_LESS_THAN:
+                p[j] = int(a < b)
+            elif op == OP_EQUALS:
+                p[j] = int(a == b)
             else:
                 raise RuntimeError("Unknown binary operation.")
             i += 4
@@ -80,12 +91,28 @@ def run(p, inputs=[]):
             outputs.append(a)
             print(a)
             i += 2
+        elif op == OP_JUMP_IF_TRUE or op == OP_JUMP_IF_FALSE:
+            if len(p) - i <= 2:
+                raise RuntimeError("Not enough arguments for jump.")
+            a = load_parameter(p, i+1, mode1)
+            j = load_parameter(p, i+2, mode2)
+            jump = (op == OP_JUMP_IF_TRUE and a) or \
+                    (op == OP_JUMP_IF_FALSE and not a)
+            if jump:
+                # Note: only validate jump address if we're actually going to
+                # jump, since the address might be bad if the condition is
+                # false.
+                if j < 0 or j >= len(p):
+                    raise RuntimeError("Invalid jump address.")
+                i = j
+            else:
+                i += 3
         else:
             raise RuntimeError("Unknown opcode.")
     return outputs
 
 def main():
-    parser = argparse.ArgumentParser(description="Day 5 part 1.")
+    parser = argparse.ArgumentParser(description="Day 5 part 2.")
     parser.add_argument("--debug", action="store_true")
     args = parser.parse_args()
     global debug
@@ -93,7 +120,7 @@ def main():
     with open('5-input.txt') as f:
         program_lines = map(lambda l: map(lambda i: int(i), l.split(',')), f)
         program = [ num for line in program_lines for num in line ]
-        run(program, [1])
+        run(program, [5])
 
 if __name__ == '__main__':
     main()
